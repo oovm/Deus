f2922f0b51e3aa0412bdf64c2e142c58c7831366
Aster <192608617@qq.com>
2016-04-09 13:12:48 +0800

    🎋 Regroup projects with mono repo

---
+++ projects/wolfram-bind/PacletInfo.m
@@ -0,0 +1,23 @@
+(* ::Package:: *)
+
+(* Created with the Wolfram Language : www.wolfram.com *)
+(* created 2017/12/05*)
+(* updated 2017/12/29*)
+Paclet[
+	Name -> "Deus",
+	Version -> "0.5.2",
+	Extensions -> {
+		{
+			"Kernel", 
+			"Root" -> ".", 
+			"Context" -> {"Deus`"},
+			Root -> ".", 
+			Context -> {"Deus`"}
+		}, 
+		{
+			"Documentation",
+			"Language" -> "English", 
+			"MainPage" -> "Guides\\Deus"
+		}
+	}
+]
