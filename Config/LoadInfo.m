784ff3ca7a14e68462452fd6bd0e4ec1abb02041
Aster <galaster@foxmail.com>
2019-08-17 14:39:36 +0800

    ❌ Remove the loader

--- Config/LoadInfo.m
+++
@@ -1,2 +0,0 @@
-(* Created with the Wolfram Language : www.wolfram.com *)
-{"PreLoad" -> None, "FEHidden" -> {}, "PackageScope" -> None}
--- Config/UploadInfo.m
+++
@@ -1,2 +0,0 @@
-(* Created with the Wolfram Language : www.wolfram.com *)
-{"ServerName" -> Automatic, CloudConnect -> False}
